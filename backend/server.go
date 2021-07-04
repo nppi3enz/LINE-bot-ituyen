32d0a4b176f2b33fc6470d69be34d9d1658d91ce
Nipitpon Chantada <nipitpon.cha@ascendcorp.com>
2021-07-04 23:26:53 +0700

    refractor code

--- backend/server.go
+++ backend/server.go
@@ -10,6 +10,8 @@ import (
 	"os"
 )
 
+var connectBot *linebot.Client
+
 func connectLineBot() *linebot.Client {
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
@@ -21,68 +23,74 @@ func connectLineBot() *linebot.Client {
 	return bot
 }
 
+func lineBot(c *gin.Context) {
+	bot := connectBot
+	events, err := bot.ParseRequest(c.Request)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			c.JSON(400, gin.H{
+				"message": "Error",
+			})
+		} else {
+			c.JSON(500, gin.H{
+				"message": "Error",
+			})
+		}
+		return
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if message.Text == "hello" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("สวัสดีค่ะ")).Do(); err != nil {
+						log.Print(err)
+					}
+				} else if message.Text == "carousel" {
+				} else if message.Text == "เช็ควันหมดอายุ" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("รออีกแปปน๊า")).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+				// if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+				// 	log.Print(err)
+				// }
+			case *linebot.StickerMessage:
+				replyMessage := fmt.Sprintf(
+					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	r.GET("/", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
 		})
 	})
 
-	bot := connectLineBot()
+	connectBot = connectLineBot()
 
-	r.POST("/callback", func(c *gin.Context) {
-		events, err := bot.ParseRequest(c.Request)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				c.JSON(400, gin.H{
-					"message": "Error",
-				})
-			} else {
-				c.JSON(500, gin.H{
-					"message": "Error",
-				})
-			}
-			return
-		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if message.Text == "hello" {
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("สวัสดีค่ะ")).Do(); err != nil {
-							log.Print(err)
-						}
-					} else if message.Text == "เช็ควันหมดอายุ" {
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("รออีกแปปน๊า")).Do(); err != nil {
-							log.Print(err)
-						}
-					}
-					// if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					// 	log.Print(err)
-					// }
-				case *linebot.StickerMessage:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-		// c.JSON(http.StatusOK, gin.H{
-		// 	"message": "callback",
-		// })
-	})
+	router.POST("/callback", lineBot)
 
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		log.Fatal(err)
-	}
-	r.Run(":" + os.Getenv("PORT"))
+	// productAPI := router.Group("/product")
+	// {
+	// 	productAPI.POST("/create", CreateProduct)
+	// }
+
+	router.Run(":" + os.Getenv("PORT"))
 }
