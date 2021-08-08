ece54ac669311ac6d262787259ab4e7c4579fb3f
Nipitpon Chantada <nipitpon.cha@ascendcorp.com>
2021-08-08 14:43:24 +0700

    Add Feature Delete Expiry by Manual

--- backend/models/expiry.go
+++ backend/models/expiry.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Expiry struct {
+	ID         string                 `json:"id"`
 	ExpireDate time.Time              `json:"expire_date"`
 	Quantity   int64                  `json:"quantity"`
 	Product    map[string]interface{} `json:"product"`
